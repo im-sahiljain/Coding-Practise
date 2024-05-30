8b6b39f7cc69a78749ec9cc79d898be3c1a4ab86
im-sahiljain <mr.sahiljain14@gmail.com>
2024-05-30 20:26:04 +0530

    Maximum SubArray Sum Optimal

--- Python/Array/Maximum SubArray Sum Optimal.py
+++ Python/Array/Maximum SubArray Sum Optimal.py
@@ -2,11 +2,11 @@ def maxsubarray(nums):
     maxsum = nums[0]
     cursum = nums[0]
 
-    for i in range(1,len(nums)):
-        cursum = max(cursum, cursum + nums[i])
-        maxsum = max(cursum,maxsum)
+    for num in nums[1:]:
+        cursum = max(num, cursum + num)
+        maxsum = max(cursum, maxsum)
     return maxsum
 
 if __name__ == "__main__":
-    nums = [-2,-3,-1,2,5,4,-3,-5]
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
     print(maxsubarray(nums))
